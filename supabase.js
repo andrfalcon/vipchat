7cb049521426dd464997ab43a3a9bf7800138408
Andrew Falcon <falconandrew2@gmail.com>
2023-12-21 22:26:11 -0800

    supabase env configured

--- supabase.js
+++ supabase.js
@@ -1,9 +1,10 @@
 import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env'
 
-const supabaseUrl = ''
-const supabaseAnonKey = ''
+const supabaseUrl = SUPABASE_URL;
+const supabaseAnonKey = SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
