f10d14e1620b7a708c5ff5a7fbc4422c4de5f50f
Andrew Falcon <falconandrew2@gmail.com>
2024-02-01 00:03:24 -0800

    join chat functionality

--- src/discover/components/joinChat.js
+++ src/discover/components/joinChat.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 import { Text, Button } from 'react-native-paper';
+import { ChatListContext } from '../../homeNavigator';
+import { supabase } from '../../../services/supabase';
 
 const JoinChat = (props) => {
+    const sharedChatData = useContext(ChatListContext);
+
+    async function handleJoinChat() {
+        const user = (await supabase.auth.getSession()).data.session.user.user_metadata.username;
+        var userGroups = (await supabase.from('users').select('groups').eq('username', user)).data[0].groups;
+        
+        userGroups.push(props.title);
+        // Add user to their newly subscribed group
+        await supabase.from('users').update({
+            groups: userGroups
+        }).eq('username', user)
+        // Updates chatList screen
+        sharedChatData.handleUserAddedToChat();
+    }
+
     return (
         <View>
             <Text style={{ color: "white" }}>{props.title}</Text>
-            <Button mode="outlined">Join Chat</Button>
+            <Button mode="outlined" onPress={handleJoinChat}>Join Chat</Button>
         </View>
     )
 }
