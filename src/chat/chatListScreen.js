f9bfa99dabb38f3c5af5678bbc4ad51925fff952
Andrew Falcon <falconandrew2@gmail.com>
2024-02-06 00:56:13 -0800

    basic chat list screen

--- src/chat/chatListScreen.js
+++ src/chat/chatListScreen.js
@@ -6,6 +6,7 @@ import { supabase } from '../../services/supabase';
 import { create } from 'react-test-renderer';
 import { ChatListContext } from '../homeNavigator'; 
 import { RFPercentage } from 'react-native-responsive-fontsize';
+import ChatRow from './components/chatRow';
 
 const ChatListScreen = () => {
     const navigation = useNavigation();
@@ -17,25 +18,41 @@ const ChatListScreen = () => {
     }, [])
 
     return (
-        <View style={{flex:1, justifyContent: "flexStart", paddingTop: "7.5%", paddingLeft: "7.5%", backgroundColor: "#14141A"}}>
-            <Text style={{ color: "white", fontFamily:"SpaceGrotesk-Bold", fontSize: RFPercentage(6) }}>Chats</Text>
-            <FlatList 
+        <View style={{flex:1, justifyContent: "flexStart", paddingTop: "7.5%", paddingLeft: "5%", backgroundColor: "#14141A"}}>
+            <Text 
+                style={{ 
+                    color: "white", 
+                    fontFamily:"SpaceGrotesk-Bold", 
+                    fontSize: RFPercentage(6),
+                    paddingBottom: "3%"
+                }}>
+                Chats
+            </Text>
+            {/* <FlatList 
                 data={sharedChatData.chatList} 
                 renderItem={({item}) => (<Button onPress={() => navigation.navigate('Chat', {
                     chatName: item.name
                 })}>{item.name}</Button>)} 
+            /> */}
+            <FlatList 
+                data={sharedChatData.chatList} 
+                renderItem={({item}) => (
+                    <ChatRow chatName={item.name} />
+                )} 
             />
-            <View style={{ alignSelf: "flex-end", width: 70, height: 70, borderRadius: 35, backgroundColor: "#742DDD", justifyContent: "center", alignItems: "center"}}>
-            <TouchableOpacity onPress={() => navigation.navigate('CreateChat')}>
-                <Image 
-                    style={{
-                        resizeMode: "cover", 
-                        aspectRatio: 1, 
-                        width: "55%", 
-                        height: undefined}} 
-                    source={require('../../assets/createChat.png')} 
-                />
-            </TouchableOpacity>
+            <View style={{position: 'absolute', top: "87%", right: "13%"}}>
+                <View style={{ width: 70, height: 70, borderRadius: 35, backgroundColor: "#742DDD", justifyContent: "center", alignItems: "center"}}>
+                    <TouchableOpacity onPress={() => navigation.navigate('CreateChat')}>
+                        <Image 
+                            style={{
+                                resizeMode: "cover", 
+                                aspectRatio: 1, 
+                                width: "55%", 
+                                height: undefined}} 
+                            source={require('../../assets/createChat.png')} 
+                        />
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     )
--- src/chat/components/chatRow.js
+++ src/chat/components/chatRow.js
@@ -1,48 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { RFPercentage } from 'react-native-responsive-fontsize';
+import { useNavigation } from '@react-navigation/native';
 import { format } from 'date-fns';
 
-// PROPS:
-// latestMessage, title, time
-
 const ChatRow = (props) => {
-    const [formattedDate, setFormattedDate] = useState('');
-
-    useEffect(() => {
-        const dateTimeObject = new Date(props.time);
-        setFormattedDate(format(dateTimeObject, "MM/dd/yy 'at' h:mm a"));
-    }, [])
+    const navigation = useNavigation();
 
     return (
-        <View style={{ flexDirection: "row", width: "95%", alignSelf: "center", paddingBottom: "1.5%" }}>
-            <Text style={{ fontSize: RFPercentage(5) }}>🧩</Text>
-            <View style={{flexDirection: "column", paddingLeft: "1.75%" }}>
-                <View style={{ flexDirection: "row" }}>
-                    <Text 
-                        style={{ 
-                            color: "#8F5CDA", 
-                            fontFamily: "Montserrat-Medium",
-                            fontWeight: "bold"
-                        }}>
-                        {props.title}
-                    </Text>
-                    <Text style={{ 
-                        color: "#626262",
-                        fontFamily: "Montserrat-Medium",
-                        paddingLeft: "3%"
-                    }}>{formattedDate}</Text>
-                </View>
-                <View style={{ width: "93%" }}>
-                    <Text
-                        style={{ 
-                            color: "white",
-                            fontFamily: "Montserrat-Medium",
-                            flexWrap: 'wrap'
-                    }}>{props.latestMessage}</Text>
-                </View>
+        <TouchableOpacity onPress={() => navigation.navigate('Chat', {
+            chatName: props.chatName
+        })}>
+            <View style={{ flexDirection: "row", paddingBottom: "3%" }}>
+                <Text style={{ fontSize: RFPercentage(5), alignSelf: "center"}}>🧩</Text>
+                    <View style={{flexDirection: "column", paddingLeft: "1.75%" }}>
+                        <View style={{ flexDirection: "row" }}>
+                            <Text 
+                                style={{ 
+                                    color: "white", 
+                                    fontFamily: "SpaceGrotesk-Bold",
+                                    fontSize: RFPercentage(2.5)
+                                }}>
+                                {props.chatName}
+                            </Text>
+                        </View>
+                        <View style={{ width: "98%" }}>
+                            <Text
+                                style={{ 
+                                    color: "white",
+                                    fontFamily: "Montserrat-SemiBold",
+                                    color: "#B8B8B8",
+                                    flexWrap: 'wrap',
+                                    fontSize: RFPercentage(2)
+                            }}>Group Chat's Latest message goes here. Group Chat's Latest message goes here. Group Chat's Latest message goes here</Text>
+                        </View>
+                    </View>
             </View>
-        </View>
+        </TouchableOpacity>
     )
 }
 
