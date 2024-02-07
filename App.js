c4ce09f35ec34449d832da39360f73e4253eae70
Andrew Falcon <falconandrew2@gmail.com>
2024-02-06 22:22:40 -0800

    test

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navigation from './src/navigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+// TEST TEST
+
 const App = () => {
   return (
     <Navigation />
